756646a37a8062e005f175711862a2162077ce84
zhangjun01 <zhangjun01@cobotsys.com>
2021-02-20 11:03:43 +0800

    valarray

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include "std_algorithm_test.h"
 
+#define SIZE 10
+
 int main() {
     std_valarray_test();
+//    timespec tn;
+//    std::valarray<double> a1(SIZE);
+//    for (int i = 0; i < SIZE; ++i) {
+//        a1[i]=rand()%10;
+//    }
+//
+//    clock_gettime(CLOCK_REALTIME,&tn);
+//    auto before=tn.tv_nsec;
+//    std::valarray<double> a2;
+//    std::copy(std::begin(a1),std::end(a1),std::insert_iterator<std::valarray<double>>(a2,std::begin(a2)));
+//    clock_gettime(CLOCK_REALTIME,&tn);
+//    auto after=tn.tv_nsec;
+//    std::cout<<"迭代器复制："<<(after-before)/1e9;
     return 0;
 }
